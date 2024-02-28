b5a004dcda69180ae54590cc2aa6b7e6ad679722
Антон Садаков <sadacov_anton@mail.ru>
2024-02-28 08:31:41 +0500

    На проверку

--- js/main.js
+++ js/main.js
@@ -3,25 +3,11 @@ const MESSAGE = [
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 const NAMES = [
   'Александр',
   'Виктория',
   'Дмитрий',
-  'Екатерина',
-  'Иван',
-  'Жанна',
-  'Константин',
-  'Лариса',
-  'Михаил',
-  'Наталья',
-  'Олег',
-  'Полина',
-  'Роман',
-  'Светлана',
-  'Тимур',
   'Ульяна',
   'Владимир',
   'Яна',
@@ -34,15 +20,7 @@ const DESCIPTION = [
   'Очаровательный закат',
   'Это место - настоящий рай на земле!',
   'Какая атмосфера!',
-  'Вот это вид! Потрясающе!',
-  'Как же здесь тихо и спокойно...',
-  'Ощущение, будто находишься в сказке!',
-  'Лучшее место для отдыха и медитации.',
   'Подарок для глаз и души!',
-  'Мечтаю вернуться сюда снова и снова.',
-  'Природа вдохновляет на великие дела.',
-  'Такой момент стоит запомнить навсегда.',
-  'Идеальное место для романтического ужина.',
   'Как прекрасен этот мир!',
   'Моменты счастья на каждом шагу.',
   'Подарок от природы.',
@@ -51,15 +29,8 @@ const DESCIPTION = [
   'Давайте насладимся этой красотой вместе!',
   'Великолепная атмосфера для творчества.',
   'Этот вид заставляет забыть обо всём на свете.',
-  'Лучший способ провести выходные!',
-  'Природа - лучший художник.',
-  'Только в природе можно найти настоящее счастье.',
-  'Моменты, которые останутся в сердце навсегда.',
-  'Здесь каждая деталь словно сказка.',
-  'Невероятно красиво и живописно!',
-  'Природа вдохновляет на великие мысли.',
-  'Счастье - это просто находиться здесь и сейчас.'
 ];
+
 //Генерация чисел
 const SIMILAR_WIZARD_COUNT = 25;
 
@@ -104,32 +75,23 @@ function createIdGenerator () {
 
 const generateId = createIdGenerator();
 
-////Генерация списока комментариев
-//const commentListGeneration = function () {
-//  const comments = [];
-//  for (let i = 1; i <= getRandomInteger(1, 30); i++) {
-//    const id = i;
-//    const avatar = `img/avatar-${getRandomInteger(1, 6)}.svg`;
-//    const name = getRandomArrayElement(NAMES);
-//    const message = getRandomArrayElement(MESSAGE);
-//    comments.push({
-//      id,
-//      avatar,
-//      message,
-//      name
-//    });
-//  }
-//  return comments;
-//};
-
-const createCommentObj = () => ({
-  id: generateId(),
-  avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
-  message: getRandomArrayElement(MESSAGE),
-  name: getRandomArrayElement(NAMES),
-});
-
-const commentsArray = Array.from({ length: getRandomInteger(1, 30) }, createCommentObj);
+//Генерация списока комментариев
+const commentListGeneration = function () {
+  const comments = [];
+  for (let i = 1; i <= getRandomInteger(1, 30); i++) {
+    const id = i;
+    const avatar = `img/avatar-${getRandomInteger(1, 6)}.svg`;
+    const name = getRandomArrayElement(NAMES);
+    const message = getRandomArrayElement(MESSAGE);
+    comments.push({
+      id,
+      avatar,
+      message,
+      name
+    });
+  }
+  return comments;
+};
 
 //Объект
 const createWizard = () => ({
@@ -137,7 +99,7 @@ const createWizard = () => ({
   url: `photos/${generatePhoto()}.jpg`,
   description: getRandomArrayElement(DESCIPTION),
   likes: getRandomInteger(15, 200),
-  comments: createCommentObj(),
+  comments: commentListGeneration(),
 });
 
 const similarWizards = Array.from({length: SIMILAR_WIZARD_COUNT}, createWizard);

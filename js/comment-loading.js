8aece202368e2d0c89b530a575dc328959dc1eaf
Антон Садаков <sadacov_anton@mail.ru>
2024-03-28 20:41:01 +0500

    ДЗ 8-2

--- js/comment-loading.js
+++ js/comment-loading.js
@@ -1,56 +1,107 @@
 import {createCommentObj} from './data.js';
-import {commentList} from './window-rendering.js';
+// import {commentList} from './window-rendering.js';
 
 const nowCommentsButton = document.querySelector('.comments-loader');
 
-const COMMENTS_NUM = 5
-const creatingCommentList  = () => {
-  const newComment = document.createElement('li');
-  newComment.classList.add('social__comment');
-  commentList.append(newComment);
+const socialComments = document.querySelector('.social__comments');
+// const socialComment = socialComments.querySelector('.social__comment');
+const socialCommentCount = document.querySelector('.social__comment-shown-count');
+const socialCommentsLoader = document.querySelector('.social__comments-loader');
 
-  const avatarImages = document.createElement('img');
-  avatarImages.classList.add('social__picture');
-  avatarImages.src = createCommentObj().avatar;
-  avatarImages.alt = createCommentObj().name;
-  newComment.append(avatarImages);
+// const commentList = document.querySelector('.social__comments');
+// const commentItem = commentList.querySelector('.social__comment');
 
-  const paragraph = document.createElement('p');
-  paragraph.classList.add('social__text');
-  paragraph.textContent = createCommentObj().message;
-  newComment.append(paragraph);
-}
+// import {generatedImages} from './thumbnail.js';
 
-export const commentLoading = (item) => {
-  let commentsItem = 0;
-  if (COMMENTS_NUM >= item.comments.length) {
-    for (let i = 0; i < item.comments.length; i++) {
-      creatingCommentList()
+// const commentList = document.querySelector('#comment').content;
+const commentListClon = document.querySelector('#comment')
+  .content
+  .querySelector('.social__comment');
+// let comments = [];
+// let currentComment = 0;
+const COMMENTS_NUM = 5;
+
+// const renderComment = createCommentObj();
+
+export const creatingCommentList = () => {
+  const fragmentComment = document.createDocumentFragment();
+  const commentElement = commentListClon.cloneNode(true);
+
+  commentElement.querySelector('.social__picture').src = createCommentObj().avatar;
+  commentElement.querySelector('.social__picture').alt = createCommentObj().name;
+  commentElement.querySelector('.social__text').textContent = createCommentObj().message;
+
+  fragmentComment.appendChild(commentElement)
+
+
+  return fragmentComment;
+};
+
+export const commentLoading = (fragmentComment) => {
+  let currentCount = 0;
+  let comments = [];
+  return () => {
+    comments = fragmentComment;
+    const COUNT_STEP = 5;
+    const arrComments = comments.slice(currentCount, currentCount + COUNT_STEP);
+    const arrCommentsLength = arrComments.length + currentCount;
+    arrComments.forEach((comment) => {
+      const commentElement = creatingCommentList(comment);
+      dataListFragment.appendChild(commentElement);
+    });
+
+    socialComments.appendChild(dataListFragment);
+    socialCommentCount.textContent = arrCommentsLength;
+    if (arrCommentsLength >= comments.length) {
+      socialCommentsLoader.classList.add('hidden');
     }
+    currentCount += COUNT_STEP;
+  };
+};
 
-    nowCommentsButton.classList.add('hidden');
-  }
 
-  if (commentsItem <= item.comments.length-5) {
-    commentsItem -= 5
-    for (let i = item.comments.length; i > commentsItem; i -= 5) {
-      creatingCommentList()
-      nowCommentsButton.addEventListener('click', () => {
-        creatingCommentList()
-      })
-    }
-    nowCommentsButton.classList.remove('hidden');
+export const initialRenderComments = (currentComment) => {
+  const render = renderComments(currentComment);
 
-    if (item.comments.length < COMMENTS_NUM) {
-      nowCommentsButton.addEventListener('click', () => {
-        for (let i = 0; i < item.comments.length; i++) {
-          creatingCommentList()
-          nowCommentsButton.classList.add('hidden');
-        }
-      })
-    }
-  }
-}
+  socialCommentsLoader.addEventListener('click', render);
+};
+
+export const clearComments = () => {
+  socialComments.innerHTML = '';
+  socialCommentCount.textContent = 0;
+  socialCommentsLoader.classList.remove('hidden');
+  socialCommentsLoader.removeEventListener('click', initialRenderComments);
+};
+
+// console.log(creatingCommentList())
+
+// export const creatingCommentList  = (comments) => {
+//   const newComment = document.createElement('li');
+//   newComment.classList.add('social__comment');
+//   commentList.append(newComment);
+
+//   const avatarImages = document.createElement('img');
+//   avatarImages.classList.add('social__picture');
+//   avatarImages.src = comments.avatar;
+//   avatarImages.alt = comments.name;
+//   newComment.append(avatarImages);
+
+//   const paragraph = document.createElement('p');
+//   paragraph.classList.add('social__text');
+//   paragraph.textContent = comments.message;
+//   newComment.append(paragraph);
+// }
+
+// export const commentLoading = (item) => {
+//   if (COMMENTS_NUM >= item.length) {
+//     for (let i = 0; i < 5; i++) {
+//       creatingCommentList()
+//     }
+
+//     nowCommentsButton.classList.add('hidden');
+//   }
+// }
+// commentLoading()
 
 //nowCommentsButton.addEventListener('click', () => {
 //  nowComment.classList.add('hidden');
